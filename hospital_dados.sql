769da3ba9dd574901626ecaca963c24274de18e7
Gleyce Pereira <142240220+GleycePereira@users.noreply.github.com>
2023-11-08 01:17:24 -0300

    Add files via upload

--- hospital_dados.sql
+++ hospital_dados.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE IF NOT EXISTS hospital_dados;
-USE hospital_dados;
+CREATE DATABASE IF NOT EXISTS hospital__dados;
+USE hospital__dados;
 
 DROP TABLE IF EXISTS especialidade;
 CREATE TABLE especialidade(
@@ -13,8 +13,8 @@ CREATE TABLE medico(
 	id_medico INT AUTO_INCREMENT PRIMARY KEY,
 	nome_med VARCHAR (100) NOT NULL,
     sexo VARCHAR (45) NOT NULL,
-    CPF VARCHAR (20) NOT NULL,
-    CRM INT (4) NOT NULL,
+    CPF VARCHAR (20) NOT NULL UNIQUE,
+    CRM INT (4) NOT NULL UNIQUE,
     data_nasc DATE NOT NULL,
     id_med_espec INT NOT NULL,
     FOREIGN KEY (id_med_espec) REFERENCES especialidade(id_especialidade) ON DELETE CASCADE ON UPDATE CASCADE
@@ -24,13 +24,13 @@ DROP TABLE IF EXISTS convenio;
 CREATE TABLE convenio(
 	id_convenio INT PRIMARY KEY AUTO_INCREMENT,
     nome_convenio VARCHAR(100) NOT NULL,
-    CNPJ VARCHAR(100) NOT NULL
+    CNPJ VARCHAR(100) NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS responsavel(
 	id_responsavel INT PRIMARY KEY AUTO_INCREMENT,
     nome_responsavel VARCHAR(100) NOT NULL,
-    CPF VARCHAR(20) NOT NULL,
+    CPF VARCHAR(20) NOT NULL UNIQUE,
     parentesco VARCHAR(20) NOT NULL
 );
 
@@ -41,7 +41,7 @@ CREATE TABLE paciente(
     nome_paci VARCHAR (100) NOT NULL,
     sexo VARCHAR (20) NOT NULL,
     RG VARCHAR(20) NOT NULL,
-    CPF VARCHAR(20) NOT NULL,
+    CPF VARCHAR(20) NOT NULL UNIQUE,
     data_nasc DATE NOT NULL,
     telefone VARCHAR (20) NOT NULL,
     email VARCHAR (100) NOT NULL,
@@ -94,8 +94,8 @@ CREATE TABLE IF NOT EXISTS quarto(
 CREATE TABLE IF NOT EXISTS enfermeiro(
 	id_enfermeiro INT PRIMARY KEY AUTO_INCREMENT,
     nome_enfermeiro VARCHAR(100) NOT NULL,
-    CPF VARCHAR (20) NOT NULL,
-    COREN INT(6) NOT NULL
+    CPF VARCHAR (20) NOT NULL UNIQUE,
+    COREN INT(6) NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS internacao(
@@ -119,7 +119,7 @@ CREATE TABLE IF NOT EXISTS internacao(
 ALTER TABLE especialidade DROP COLUMN cnpj;
 
 -- Especialidade
-INSERT INTO `hospital_dados`.`especialidade` (`nome_especialidade`) VALUES
+INSERT INTO `hospital__dados`.`especialidade` (`nome_especialidade`) VALUES
 ("Clínico geral"),
 ("Gastroenterologia"),
 ("Pediatria"),
@@ -129,7 +129,7 @@ INSERT INTO `hospital_dados`.`especialidade` (`nome_especialidade`) VALUES
 ("Neurologista");
 
 -- Medico
-INSERT INTO `hospital_dados`.`medico`(`nome_med`,`sexo`,`CPF`,`CRM`,`data_nasc`,`id_med_espec`) VALUES
+INSERT INTO `hospital__dados`.`medico`(`nome_med`,`sexo`,`CPF`,`CRM`,`data_nasc`,`id_med_espec`) VALUES
 ("Caio Andrade","Masculino","567.935.012-84","3920","1998-04-23",6),
 ("Fernada Souto","Feminino","649.234.901-24","3915","1990-07-12",1),
 ("Camila Santana","Feminino","456.967.234-22","4509","1988-03-01",3),
@@ -144,21 +144,21 @@ INSERT INTO `hospital_dados`.`medico`(`nome_med`,`sexo`,`CPF`,`CRM`,`data_nasc`,
 ("Amanda Belo","Feminino","722.580.810-92","6526","1951-01-12",7);
 
 -- Convenio
-INSERT INTO `hospital_dados`.`convenio`(`nome_convenio`,`CNPJ`)VALUES
+INSERT INTO `hospital__dados`.`convenio`(`nome_convenio`,`CNPJ`)VALUES
 ("Saúde Mais","30.591.510/0001-41"),
 ("Agil","18.978.822/0001-07"),
 ("Sul Novaes","71.940.694/0001-95"),
 ("Porta Segura","14.225.407/0001-21");
 
 -- Responsavel
-INSERT INTO `hospital_dados`.`responsavel`(`nome_responsavel`,`CPF`,`parentesco`) VALUES
+INSERT INTO `hospital__dados`.`responsavel`(`nome_responsavel`,`CPF`,`parentesco`) VALUES
 ("Carlos Costa","596.756.020-31", "Pai"),
 ("Sara Andrade","102.182.760-60", "Mãe"),
 ("Fernanda Nascimento","521.021.380-38", "Filha"),
 ("Daniel Campos","840.995.980-12", "Sobrinho");
 
 -- Paciente 
-INSERT INTO `hospital_dados`.`paciente`(`nome_paci`,`sexo`,`RG`,`CPF`,`data_nasc`,`telefone`,`email`,`endereco`,`id_paci_conv`,`id_responsavel`) VALUES
+INSERT INTO `hospital__dados`.`paciente`(`nome_paci`,`sexo`,`RG`,`CPF`,`data_nasc`,`telefone`,`email`,`endereco`,`id_paci_conv`,`id_responsavel`) VALUES
 ("Erica Gusmão","Femino","42.109.856-3","698.213.094-23","1989-03-21","(11)99438-0321","erica23@g.com","R Falcão Nero 28", NULL,NULL),
 ("Gustavo Dias","Masculino","72.187.856-2","398.215.004-29","1999-12-05","(11)93458-1236","dias_gu@mail.com","R Vasconcelos Pinho, 344", NULL,NULL),
 ("Maria Helena Costa","Feminino","98.765.309-5","745.602.218-45","2012-07-08","(11)97643-0841","carlos_costa@g.com","Av Castro Alves, 901", 4,1),
@@ -176,7 +176,7 @@ INSERT INTO `hospital_dados`.`paciente`(`nome_paci`,`sexo`,`RG`,`CPF`,`data_nasc
 ("José Rocha","Masculino","45.796.611-7","913.326.560-72","1955-08-25","(11)97644-3766","rocha_jose@mail.com","R Canapés 72",3,NULL);
 
 -- CONSULTA
-INSERT INTO `hospital_dados`.`consulta`(`data_consulta`, `id_med`, `id_paci`, `hora`, `valor`) VALUES 
+INSERT INTO `hospital__dados`.`consulta`(`data_consulta`, `id_med`, `id_paci`, `hora`, `valor`) VALUES 
 ('2015-03-10', 2, 15, '09:00', 95.00),
 ('2017-06-22', 5, 6, '10:30', 135.00), 
 ('2016-01-14', 1, 7, '11:15', 130.00),
@@ -199,7 +199,7 @@ INSERT INTO `hospital_dados`.`consulta`(`data_consulta`, `id_med`, `id_paci`, `h
 ('2021-01-01', 12, 3, '17:30', 130.00);
 
 -- Receita
-INSERT INTO `hospital_dados`.`receita`(`medicamento`,`id_cMedi`,`id_pMedi`,`id_mMedi`)VALUES
+INSERT INTO `hospital__dados`.`receita`(`medicamento`,`id_cMedi`,`id_pMedi`,`id_mMedi`)VALUES
 ("Flanax 550mg 2xdia",1,15,2),
 ("Voltaren 75mg 3xdia",1,15,2),
 ("Pyridium 200mg 8em8h",3,7,1),
@@ -223,19 +223,19 @@ INSERT INTO `hospital_dados`.`receita`(`medicamento`,`id_cMedi`,`id_pMedi`,`id_m
 ("Paracetamol 15mg 4em4h",10,1,2);
 
 -- Tipo quarto
-INSERT INTO `hospital_dados`.`tipo_quarto`(`descricao`,`valor_diaria`)VALUES
+INSERT INTO `hospital__dados`.`tipo_quarto`(`descricao`,`valor_diaria`)VALUES
 ("Enfermaria", 100),
 ("Quartos duplos", 120),
 ("Apartamento", 180);
 
 -- Quarto
-INSERT INTO `hospital_dados`.`quarto`(`numero_quarto`,`id_tipo`)VALUES
+INSERT INTO `hospital__dados`.`quarto`(`numero_quarto`,`id_tipo`)VALUES
 (33, 1),
 (35, 2),
 (52, 3);
 
 -- Enfermeiro
-INSERT INTO `hospital_dados`.`enfermeiro`(`nome_enfermeiro`,`CPF`,`COREN`)VALUES
+INSERT INTO `hospital__dados`.`enfermeiro`(`nome_enfermeiro`,`CPF`,`COREN`)VALUES
 ("Aline Oliveira","141.369.040-88","290765"),
 ("Renata Campos","181.756.620-26","346894"),
 ("Rodrigo Vasconcelos","642.915.220-02","672169"),
@@ -247,9 +247,8 @@ INSERT INTO `hospital_dados`.`enfermeiro`(`nome_enfermeiro`,`CPF`,`COREN`)VALUES
 ("Lucas Modesto","801.322.580-13","180325"),
 ("Joaquim Mello","611.037.450-42","487120");
 
-
 -- Internacao
-INSERT INTO `hospital_dados`.`internacao`(`procedimento`,`data_entrada`,`data_prev_alta`,`data_alta`,`id_pacIn`,`id_medIn`,`id_quartoIn`,`id_enferUm`,`id_enferDois`)
+INSERT INTO `hospital__dados`.`internacao`(`procedimento`,`data_entrada`,`data_prev_alta`,`data_alta`,`id_pacIn`,`id_medIn`,`id_quartoIn`,`id_enferUm`,`id_enferDois`)
 VALUES
 ("Lorem Ipsum is simply dummy text", "2015-10-20", "2015-11-2","2015-11-2",12,9,3,1,4),
 ("Lorem Ipsum is simply dummy text", "2020-10-29", "2020-11-5","2020-11-3",3,3,2,7,10),
@@ -265,3 +264,128 @@ ALTER TABLE medico ADD COLUMN em_atividade VARCHAR(100);
 
 UPDATE medico SET em_atividade = 'ativo' WHERE id_medico BETWEEN 1 AND 10;
 UPDATE medico SET em_atividade = 'inativo' WHERE id_medico IN (11, 12);
+
+
+INSERT INTO `hospital__dados`.`medico`(`nome_med`,`sexo`,`CPF`,`CRM`,`data_nasc`,`id_med_espec`,`em_atividade`) VALUES
+("Paloma Fernandes","Feminino","530.519.660-45","8331","1971-03-12",7,"ativo"),
+("Gabriel Azevedo","Masculino","555.266.900-24","9944","1989-10-08",3,"ativo"),
+("Daniela Coelho","Feminino","489.734.910-91","2245","1959-01-22",1,"inativo"),
+("Gabriel D'avila","Masculino","712.272.760-28","0092","2000-12-04",6,"ativo");
+
+
+-- Consultas de 2020 e que foram feitas sob o convenio
+SELECT
+    paciente.id_paciente,
+    paciente.nome_paci,
+	paciente.id_paci_conv,
+    medico.id_medico,
+    medico.nome_med,
+    consulta.id_consulta,
+    consulta.data_consulta,
+    consulta.valor
+FROM
+    consulta 
+JOIN paciente ON paciente.id_paciente = consulta.id_paci
+JOIN medico ON consulta.id_med = medico.id_medico
+JOIN convenio  
+WHERE
+    YEAR(consulta.data_consulta) = 2020;
+
+
+--  Internações que tiveram data de alta maior que a data prevista
+SELECT * FROM internacao
+WHERE data_alta > data_prev_alta;
+
+
+-- Receituário da primeira consulta registrada
+SELECT *FROM receita
+WHERE id_cMedi = 1;
+
+
+-- Dados do maior e menor valor da consulta e não podem ser realizadas pelo convenio
+(
+    SELECT * FROM consulta
+    WHERE id_paci NOT IN (SELECT id_paciente FROM paciente WHERE id_paci_conv IS NOT NULL)
+    ORDER BY valor DESC
+    LIMIT 1
+)
+UNION
+(
+    SELECT *
+    FROM consulta
+    WHERE id_paci NOT IN (SELECT id_paciente FROM paciente WHERE id_paci_conv IS NOT NULL)
+    ORDER BY valor
+    LIMIT 1
+);
+
+
+-- Todas as internações em seus quartos, calculando o total da internação de acordo com a diária e número de dias internados
+SELECT internacao.id_internacao, internacao.procedimento, internacao.data_entrada, internacao.data_alta, 
+       quarto.numero_quarto, tipo_quarto.valor_diaria,
+       DATEDIFF(internacao.data_alta, internacao.data_entrada) AS dias_internados,
+       DATEDIFF(internacao.data_alta, internacao.data_entrada) * tipo_quarto.valor_diaria AS custo_total
+FROM internacao 
+JOIN quarto ON internacao.id_quartoIn = quarto.id_quarto
+JOIN tipo_quarto ON quarto.id_tipo = tipo_quarto.id_tipo_quarto
+ORDER BY custo_total;
+-- DATEDIFF é uma função em que calcula a diferença entre duas datas
+
+
+-- Data, procedimento e número de quarto de internações em quartos do tipo apartamento
+SELECT internacao.data_entrada, internacao.procedimento, quarto.numero_quarto
+FROM internacao
+INNER JOIN quarto ON internacao.id_quartoIn = quarto.id_quarto
+INNER JOIN tipo_quarto ON quarto.id_tipo = tipo_quarto.id_tipo_quarto
+WHERE tipo_quarto.descricao = 'apartamento';
+
+
+-- Nome do paciente, data da consulta e especialidade de todas as consultas em que os pacientes eram menores de 18 anos cuja especialidade não seja “pediatria”
+SELECT paciente.nome_paci, consulta.data_consulta, especialidade.nome_especialidade
+FROM paciente
+JOIN consulta ON paciente.id_paciente = consulta.id_paci
+JOIN medico ON consulta.id_med = medico.id_medico
+JOIN especialidade ON medico.id_med_espec = especialidade.id_especialidade
+WHERE paciente.data_nasc > DATE_SUB(CURDATE(), INTERVAL 18 YEAR)
+AND especialidade.nome_especialidade != 'Pediatria' -- Diferente
+ORDER BY consulta.data_consulta;
+-- A função DATE_SUB para obter a data há 18 anos a partir da data atual.
+-- O CURDATE() fornece a data atual.
+-- O INTERVAL 18 YEAR subtrai 18 anos dessa data atual.
+-- Isso resulta na data há exatamente 18 anos antes da data atual.
+
+
+-- Paciente, médico, data da internação e procedimentos das internações realizadas por médicos da especialidade “gastroenterologia”, que tenham acontecido em “enfermaria”.
+SELECT paciente.nome_paci, medico.nome_med, internacao.data_entrada, internacao.procedimento
+FROM paciente
+INNER JOIN internacao ON paciente.id_paciente = internacao.id_pacIn
+INNER JOIN medico ON internacao.id_medIn = medico.id_medico
+INNER JOIN especialidade ON medico.id_med_espec = especialidade.id_especialidade
+INNER JOIN quarto ON internacao.id_quartoIn = quarto.id_quarto
+INNER JOIN tipo_quarto ON quarto.id_tipo = tipo_quarto.id_tipo_quarto
+WHERE especialidade.nome_especialidade = 'Gastroenterologia'
+AND tipo_quarto.descricao = 'Enfermaria';
+
+-- Os médicos, seus CRMs e a quantidade de consultas que cada um realizou.
+SELECT medico.nome_med, medico.CRM, COUNT(consulta.id_consulta) AS total_consultas
+FROM medico
+LEFT JOIN consulta ON medico.id_medico = consulta.id_med
+GROUP BY medico.id_medico;
+-- O uso do LEFT JOIN assegura que todos os médicos sejam listados, mesmo que não tenham consultas registradas.
+-- Resultado da função COUNT(consulta.id_consulta), que irá contar o número de ocorrências na tabela consulta para cada médico. 
+-- O resultado será denominado de total_consultas.
+-- O GROUP BY é responsável por criar grupos separados para cada valor único de id_medico. 
+-- Isso significa que todas as consultas associadas ao mesmo médico serão agregadas juntas.
+
+
+-- Todos os médicos que tenham "Gabriel" no nome. 
+SELECT * FROM medico
+WHERE nome_med LIKE '%Gabriel%';
+
+-- Os nomes, CREs e número de internações de enfermeiros que participaram de mais de uma internação.
+SELECT enfermeiro.nome_enfermeiro, enfermeiro.CPF, enfermeiro.COREN, COUNT(internacao.id_internacao) AS numero_internacoes
+FROM enfermeiro
+JOIN internacao ON enfermeiro.id_enfermeiro = internacao.id_enferUm OR enfermeiro.id_enfermeiro = internacao.id_enferDois
+GROUP BY enfermeiro.id_enfermeiro
+HAVING COUNT(internacao.id_internacao) > 1;
+-- O HAVING COUNT(i.id_internacao) > 1 é uma cláusula que filtra o resultado para mostrar apenas os enfermeiros que participaram de mais de uma internação. 
+-- Isso é alcançado contando o número de internações para cada enfermeiro e selecionando apenas aqueles com mais de uma internação.
